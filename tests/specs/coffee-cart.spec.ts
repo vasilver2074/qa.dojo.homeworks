c4cbe3d28269ac7ae20f8c101294924e63bfc836
Valerii Silvanovych <vasilver@gmail.com>
2025-10-08 15:47:22 +0300

    add description

--- tests/specs/coffee-cart.spec.ts
+++ tests/specs/coffee-cart.spec.ts
@@ -1,71 +1,135 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Coffee-cart tests", () => {
-  test("PS-001 Verify Mocha is successfully added to Total", async ({ page }) => {
-    await page.goto("https://coffee-cart.app/");
-    await page.locator('[data-test="Mocha"]').click();
-    await expect(page.locator('[data-test="checkout"]')).toBeVisible();
-    await expect(page.locator('[data-test="checkout"]')).toContainText("$8.00");
-    await page.locator('[data-test="checkout"]').hover();
-    await expect(page.locator("#app")).toContainText("Mocha x 1+-");
-    //expect(page.locator('[data-test="checkout"]').innerText).toEqual(expect.stringContaining('$8.00'));
-  });
+  test(
+    "PS-001 Verify Mocha is successfully added to Total",
+    {
+      tag: ["@positive"],
+      annotation: {
+        type: "description",
+        description: "Mocha coffee is successfully added to Total",
+      },
+    },
+    async ({ page }) => {
+      await page.goto("https://coffee-cart.app/");
+      await page.locator('[data-test="Mocha"]').click();
+      await expect(page.locator('[data-test="checkout"]')).toBeVisible();
+      await expect(page.locator('[data-test="checkout"]')).toContainText(
+        "$8.00"
+      );
+      await page.locator('[data-test="checkout"]').hover();
+      await expect(page.locator("#app")).toContainText("Mocha x 1+-");
+    }
+  );
 
-  test("PS-002 Verify Payment details of adding Cappuccino is successfully sent from Menu page", async ({ page }) => {
-    await page.goto("https://coffee-cart.app/");
-    await page.locator('[data-test="Cappuccino"]').click();
-    await expect(page.locator('[data-test="checkout"]')).toBeVisible();
-    await page.locator('[data-test="checkout"]').click();
-    await page.getByRole("textbox", { name: "Name" }).fill("test");
-    await page.getByRole("textbox", { name: "Email" }).fill("test@gmail.com");
-    await page.getByRole("checkbox", { name: "Promotion checkbox" }).check();
-    await expect(
-      page.getByRole("heading", { name: "Payment details" })
-    ).toBeVisible();
-    await page.getByRole("button", { name: "Submit" }).click();
-    await expect(
-      page.getByRole("heading", { name: "Payment details" })
-    ).not.toBeVisible();
-  });
+  test(
+    "PS-002 Verify Payment details of adding Cappuccino is successfully sent from Menu page",
+    {
+      tag: ["@positive"],
+      annotation: {
+        type: "description",
+        description:
+          "Cappuccino coffee is added to Total and Payment details form is successfully sent",
+      },
+    },
+    async ({ page }) => {
+      await page.goto("https://coffee-cart.app/");
+      await page.locator('[data-test="Cappuccino"]').click();
+      await expect(page.locator('[data-test="checkout"]')).toBeVisible();
+      await page.locator('[data-test="checkout"]').click();
+      await page.getByRole("textbox", { name: "Name" }).fill("test");
+      await page.getByRole("textbox", { name: "Email" }).fill("test@gmail.com");
+      await page.getByRole("checkbox", { name: "Promotion checkbox" }).check();
+      await expect(
+        page.getByRole("heading", { name: "Payment details" })
+      ).toBeVisible();
+      await page.getByRole("button", { name: "Submit" }).click();
+      await expect(
+        page.getByRole("heading", { name: "Payment details" })
+      ).not.toBeVisible();
+    }
+  );
 
-  test("PS-003 Verify Americano is successfully is added to Cart page", async ({ page }) => {
-    await page.goto("https://coffee-cart.app/");
-    await page.locator('[data-test="Americano"]').click();
-    await expect(page.locator('[data-test="checkout"]')).toBeVisible();
-    await expect(page.locator('[data-test="checkout"]')).toContainText("$7.00");
-    await page.getByRole('link', { name: 'Cart page' }).click();
-    await expect(page.locator('[data-test="checkout"]')).toBeVisible();
-    await expect(page.locator('[data-test="checkout"]')).toContainText("$7.00");
-  });
+  test(
+    "PS-003 Verify Americano is successfully added to Cart page",
+    {
+      tag: ["@positive"],
+      annotation: {
+        type: "description",
+        description: "Americano coffee is successfully added to Cart page",
+      },
+    },
+    async ({ page }) => {
+      await page.goto("https://coffee-cart.app/");
+      await page.locator('[data-test="Americano"]').click();
+      await expect(page.locator('[data-test="checkout"]')).toBeVisible();
+      await expect(page.locator('[data-test="checkout"]')).toContainText(
+        "$7.00"
+      );
+      await page.getByRole("link", { name: "Cart page" }).click();
+      await expect(page.locator('[data-test="checkout"]')).toBeVisible();
+      await expect(page.locator('[data-test="checkout"]')).toContainText(
+        "$7.00"
+      );
+    }
+  );
 
-  test("PS-004 Verify Payment details of adding Flat White is successfully sent from Menu page", async ({ page }) => {
-    await page.goto("https://coffee-cart.app/");
-    await page.locator('[data-test="Flat_White"]').click();
-    await expect(page.locator('[data-test="checkout"]')).toBeVisible();
-    await expect(page.locator('[data-test="checkout"]')).toContainText("$18.00");
-    await page.getByRole('link', { name: 'Cart page' }).click();
-    await expect(page).toHaveTitle(/Coffee cart/);
-    await expect(page.locator('[data-test="checkout"]')).toBeVisible();
-    await expect(page.locator('[data-test="checkout"]')).toContainText("$18.00");
-    await page.locator('[data-test="checkout"]').click();
-    await page.getByRole("textbox", { name: "Name" }).fill("test");
-    await page.getByRole("textbox", { name: "Email" }).fill("test@gmail.com");
-    await page.getByRole("checkbox", { name: "Promotion checkbox" }).check();
-    await expect(
-      page.getByRole("heading", { name: "Payment details" })
-    ).toBeVisible();
-    await page.getByRole("button", { name: "Submit" }).click();
-    await expect(
-      page.getByRole("heading", { name: "Payment details" })
-    ).not.toBeVisible();
-  });
+  test(
+    "PS-004 Verify Payment details of adding Flat White is successfully sent from Menu page",
+    {
+      tag: ["@positive"],
+      annotation: {
+        type: "description",
+        description:
+          "Flat White is successfully added to Total and Payment details form is successfully sent from Menu page",
+      },
+    },
+    async ({ page }) => {
+      await page.goto("https://coffee-cart.app/");
+      await page.locator('[data-test="Flat_White"]').click();
+      await expect(page.locator('[data-test="checkout"]')).toBeVisible();
+      await expect(page.locator('[data-test="checkout"]')).toContainText(
+        "$18.00"
+      );
+      await page.getByRole("link", { name: "Cart page" }).click();
+      await expect(page).toHaveTitle(/Coffee cart/);
+      await expect(page.locator('[data-test="checkout"]')).toBeVisible();
+      await expect(page.locator('[data-test="checkout"]')).toContainText(
+        "$18.00"
+      );
+      await page.locator('[data-test="checkout"]').click();
+      await page.getByRole("textbox", { name: "Name" }).fill("test");
+      await page.getByRole("textbox", { name: "Email" }).fill("test@gmail.com");
+      await page.getByRole("checkbox", { name: "Promotion checkbox" }).check();
+      await expect(
+        page.getByRole("heading", { name: "Payment details" })
+      ).toBeVisible();
+      await page.getByRole("button", { name: "Submit" }).click();
+      await expect(
+        page.getByRole("heading", { name: "Payment details" })
+      ).not.toBeVisible();
+    }
+  );
 
-  test('PS-005 After adding 3 coffee to cart appear Extra coffee message', async ({ page }) => {
-  await page.goto('https://coffee-cart.app/');
-  await page.locator('[data-test="Cappuccino"]').click();
-  await page.locator('[data-test="Mocha"]').click();
-  await page.locator('[data-test="Flat_White"]').click();
-  await expect(page.getByText('It\'s your lucky day! Get an')).toBeVisible();
-  await expect(page.locator('#app')).toContainText('It\'s your lucky day! Get an extra cup of Mocha for $4.');
-});
+  test(
+    "PS-005 After adding 3 coffee to cart appear Extra coffee message",
+    {
+      tag: ["@positive"],
+      annotation: {
+        type: "description",
+        description:
+          "Cappuccino, Mocha and Flat_White coffee are successfully added to Total and congratulation message is displayed",
+      },
+    },
+    async ({ page }) => {
+      await page.goto("https://coffee-cart.app/");
+      await page.locator('[data-test="Cappuccino"]').click();
+      await page.locator('[data-test="Mocha"]').click();
+      await page.locator('[data-test="Flat_White"]').click();
+      await expect(page.getByText("It's your lucky day! Get an")).toBeVisible();
+      await expect(page.locator("#app")).toContainText(
+        "It's your lucky day! Get an extra cup of Mocha for $4."
+      );
+    }
+  );
 });
--- tests/specs/playwright.spec.ts
+++ tests/specs/playwright.spec.ts
@@ -1,60 +1,104 @@
 import { test, expect, chromium } from "@playwright/test";
 
 test.describe("Playwright dev tests", () => {
-  test("PS-001 Verify user is able to switch between dark and light", async ({
-    page,
-  }) => {
-    await page.goto("https://playwright.dev/");
-    await page
-      .getByRole("button", { name: "Switch between dark and light" })
-      .dblclick();
-    await expect
-      .soft(page.getByRole("button", { name: "Switch between dark and light" }))
-      .toHaveAttribute("title", "dark mode");
-  });
+  test(
+    "PS-001 Verify user is able to switch between dark and light mode",
+    {
+      tag: ["@positive"],
+      annotation: {
+        type: "description",
+        description: "After double click Playwright site displayed with dark mode",
+      },
+    },
+    async ({ page }) => {
+      await page.goto("https://playwright.dev/");
+      await page
+        .getByRole("button", { name: "Switch between dark and light" })
+        .dblclick();
+      await expect
+        .soft(
+          page.getByRole("button", { name: "Switch between dark and light" })
+        )
+        .toHaveAttribute("title", "dark mode");
+    }
+  );
 
-  test("PS-002 Verify user is able to navigate to Discord page", async ({
-    page,
-  }) => {
-    await page.goto("https://playwright.dev/");
-    await page.getByRole("link", { name: "Discord server" }).click();
-    const pagePromise = page.waitForEvent("popup");
-    const newTab = await pagePromise;
-    await newTab.waitForLoadState();
-    await expect(newTab).toHaveTitle(/Playwright - Discord Servers/);
-  });
+  test(
+    "PS-002 Verify user is able to navigate to Discord page",
+    {
+      tag: ["@positive"],
+      annotation: {
+        type: "description",
+        description: "After tap on Discord server link user was navigated to Discord Servers site",
+      },
+    },
+    async ({ page }) => {
+      await page.goto("https://playwright.dev/");
+      await page.getByRole("link", { name: "Discord server" }).click();
+      const pagePromise = page.waitForEvent("popup");
+      const newTab = await pagePromise;
+      await newTab.waitForLoadState();
+      await expect(newTab).toHaveTitle(/Playwright - Discord Servers/);
+    }
+  );
 
-  test("PS-003 Verify user is able to navigate with Main menu", async ({
-    page,
-  }) => {
-    await page.goto("https://playwright.dev/");
-    await page.getByRole("link", { name: "Docs" }).click();
-    await expect(page.getByRole("link", { name: "Docs" })).toBeVisible();
-    await expect(page.getByLabel("Main", { exact: true })).toContainText(
-      "Docs"
-    );
-    await expect(
-      page.getByRole("heading", { name: "Installation" })
-    ).toBeVisible();
+  test(
+    "PS-003 Verify user is able to navigate with Main menu",
+    {
+      tag: ["@positive"],
+      annotation: {
+        type: "description",
+        description: "User is able to navigate with Main menu",
+      },
+    },
+    async ({ page }) => {
+      await page.goto("https://playwright.dev/");
+      await page.getByRole("link", { name: "Docs" }).click();
+      await expect(page.getByRole("link", { name: "Docs" })).toBeVisible();
+      await expect(page.getByLabel("Main", { exact: true })).toContainText(
+        "Docs"
+      );
+      await expect(
+        page.getByRole("heading", { name: "Installation" })
+      ).toBeVisible();
 
-    await page.getByText('API', { exact: true }).click();
-    await expect.soft(page.getByText('API', { exact: true })).toBeVisible();
-    await expect.soft(page.getByLabel("Main", { exact: true })).toContainText("API");
-    await expect.soft(
-      page.getByRole("heading", { name: "Playwright Library" })
-    ).toBeVisible();
+      await page.getByText("API", { exact: true }).click();
+      await expect.soft(page.getByText("API", { exact: true })).toBeVisible();
+      await expect
+        .soft(page.getByLabel("Main", { exact: true }))
+        .toContainText("API");
+      await expect
+        .soft(page.getByRole("heading", { name: "Playwright Library" }))
+        .toBeVisible();
 
-    await page.getByRole("link", { name: "Community" }).click();
-    await expect(page.getByLabel("Main", { exact: true })).toContainText(
-      "Community"
-    );
-  });
+      await page.getByRole("link", { name: "Community" }).click();
+      await expect(page.getByLabel("Main", { exact: true })).toContainText(
+        "Community"
+      );
+    }
+  );
 
-  test("PS-004 Verify user is able to perform a search", async ({ page }) => {
-    await page.goto("https://playwright.dev/");
-    await page.getByRole('button', { name: 'Search (Ctrl+K)' }).click();
-    await page.getByRole('searchbox', { name: 'Search' }).fill('installation');
-    await page.getByRole('link', { name: 'Installation', exact: true }).click();
-    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-  });
+  test(
+    "PS-004 Verify user is able to perform a search",
+    {
+      tag: ["@positive"],
+      annotation: {
+        type: "description",
+        description: "user is able to perform a search Installation",
+      },
+    },
+    async ({ page }) => {
+      await page.goto("https://playwright.dev/");
+      await page.getByRole("button", { name: "Search (Ctrl+K)" }).click();
+      await page
+        .getByRole("searchbox", { name: "Search" })
+        .fill("installation");
+      await page
+        .getByRole("link", { name: "Installation", exact: true })
+        .click();
+      await expect(
+        page.getByRole("heading", { name: "Installation" })
+      ).toBeVisible();
+    }
+  );
 });
