fe86d7fe4e1e7d99fcd819300ed96616ba3f233c
Valerii Silvanovych <vasilver@gmail.com>
2025-10-23 15:50:38 +0300

    fix click with check

--- tests/specs/demo-qa.spec.ts
+++ tests/specs/demo-qa.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-import data from "../utils/data.json"
+import data from "../utils/data.json";
 
 test.describe("demo-qa tests", () => {
   test.beforeEach(async ({ page }) => {
@@ -24,10 +24,13 @@ test.describe("demo-qa tests", () => {
       const name = page.locator('//*[@id="name"]');
       const email_check = page.locator('//*[@id="email"]');
       const currentAddress_check = page.locator('//p[@id="currentAddress"]');
-      const permanentAddress_check = page.locator('//p[@id="permanentAddress"]');
+      const permanentAddress_check = page.locator(
+        '//p[@id="permanentAddress"]'
+      );
 
       await page.goto("https://demoqa.com/text-box");
-      const {full_name, email, current_address, permanent_address} = data.user;
+      const { full_name, email, current_address, permanent_address } =
+        data.user;
       await userName.fill(full_name);
       await userEmail.fill(email);
       await currentAddress.fill(current_address);
@@ -37,11 +40,10 @@ test.describe("demo-qa tests", () => {
       await expect.soft(email_check).toBeVisible();
       await expect.soft(currentAddress_check).toBeVisible();
       await expect.soft(permanentAddress_check).toBeVisible();
-      await expect.soft(name).toContainText('John Black');
-      await expect.soft(email_check).toContainText('test@gmail.com');
-      await expect.soft(currentAddress_check).toContainText('New York');
-      await expect.soft(permanentAddress_check).toContainText('Tokio');
-    
+      await expect.soft(name).toContainText("John Black");
+      await expect.soft(email_check).toContainText("test@gmail.com");
+      await expect.soft(currentAddress_check).toContainText("New York");
+      await expect.soft(permanentAddress_check).toContainText("Tokio");
     }
   );
 
@@ -56,26 +58,26 @@ test.describe("demo-qa tests", () => {
     },
     async ({ page }) => {
       const checkValidation = page.locator('//p[@class="mt-3"]/span');
+      const yes = page.locator('//input[@id="yesRadio"]');
+      const impressive = page.locator('//input[@id="impressiveRadio"]');
+      const no = page.locator('//label[@class="custom-control-label disabled"]');
 
       await page.goto("https://demoqa.com/radio-button");
-      await page.getByText('Yes').click();
-      //page.locator('//input[@id="yesRadio"]').click();
-      await expect(page.locator('label').filter({ hasText: 'Yes' })).toBeVisible();
-      await expect(checkValidation).toHaveText('Yes');
+      await expect(yes).toBeVisible();
+      await expect(yes).not.toBeChecked();
+      //await yes.check();
+      await page.getByText("Yes").check()
+      await expect.soft(yes).toBeChecked();
+      await expect.soft(checkValidation).toHaveText("Yes");
 
-      await page.getByText('Impressive').click();
-      await expect.soft(page.getByRole('radio', { name: 'Impressive' })).toBeVisible();
-      await expect.soft(checkValidation).toHaveText('Impressive');
+      await expect(impressive).toBeVisible();
+      await expect(impressive).not.toBeChecked();
+      //await impressive.check();
+      await page.getByText("Impressive").check();
+      await expect.soft(impressive).toBeChecked();
+      await expect.soft(checkValidation).toHaveText("Impressive");
 
-      await expect(page.getByText('No')).toBeVisible();
-      
-
-      
-      //await page.locator('//input[@id="yesRadio"]').click()
-      //await page.locator('//label[normalize-space()="Yes"]/preceding-sibling::input[@type="radio"]').check(); //*[@id="app"]/div/div/div/div[2]/div[2]/div[2]/label/text()
-      //await page.locator('//input[@id="yesRadio"]').check()
-      //await expect(page.locator('//label[text()="Yes"]/preceding-sibling::input')).toBeChecked();
-     
+      await expect(no).not.toBeChecked();
     }
   );
 
@@ -97,65 +99,87 @@ test.describe("demo-qa tests", () => {
       const toggleButton5 = page.locator('(//button[@title="Toggle"])[5]');
       const toggleButton6 = page.locator('(//button[@title="Toggle"])[6]');
 
-      const checkboxExcelFile = page.locator('//label[@for="tree-node-excelFile"]/span[@class="rct-checkbox"]');
-      const verifyDisplaying = page.locator('//*[@id="result"]/span[@class="text-success"]');
-      const verifyDisplayingDownloads = page.locator('//*[@id="result"]//span[text()="downloads"]');
-      const verifyDisplayingOffice = page.locator('//*[@id="result"]//span[text()="office"]');
-      const checkboxWordFile = page.locator('//label[@for="tree-node-wordFile"]/span[@class="rct-checkbox"]');
-      const checkboxDownloads = page.locator('//label[@for="tree-node-downloads"]/span[@class="rct-checkbox"]');
-      const checkboxGeneral = page.locator('//label[@for="tree-node-general"]/span[@class="rct-checkbox"]');
-      const checkboxClassified = page.locator('//label[@for="tree-node-classified"]/span[@class="rct-checkbox"]');
-      const checkboxPrivate = page.locator('//label[@for="tree-node-private"]/span[@class="rct-checkbox"]');
-      const checkboxPublic = page.locator('//label[@for="tree-node-public"]/span[@class="rct-checkbox"]');
-      const checkboxOffice = page.locator('//label[@for="tree-node-office"]/span[@class="rct-checkbox"]');
-  
+      const checkboxExcelFile = page.locator(
+        '//label[@for="tree-node-excelFile"]/span[@class="rct-checkbox"]'
+      );
+      const verifyDisplaying = page.locator(
+        '//*[@id="result"]/span[@class="text-success"]'
+      );
+      const verifyDisplayingDownloads = page.locator(
+        '//*[@id="result"]//span[text()="downloads"]'
+      );
+      const verifyDisplayingOffice = page.locator(
+        '//*[@id="result"]//span[text()="office"]'
+      );
+      const checkboxWordFile = page.locator(
+        '//label[@for="tree-node-wordFile"]/span[@class="rct-checkbox"]'
+      );
+      const checkboxDownloads = page.locator(
+        '//label[@for="tree-node-downloads"]/span[@class="rct-checkbox"]'
+      );
+      const checkboxGeneral = page.locator(
+        '//label[@for="tree-node-general"]/span[@class="rct-checkbox"]'
+      );
+      const checkboxClassified = page.locator(
+        '//label[@for="tree-node-classified"]/span[@class="rct-checkbox"]'
+      );
+      const checkboxPrivate = page.locator(
+        '//label[@for="tree-node-private"]/span[@class="rct-checkbox"]'
+      );
+      const checkboxPublic = page.locator(
+        '//label[@for="tree-node-public"]/span[@class="rct-checkbox"]'
+      );
+      const checkboxOffice = page.locator(
+        '//label[@for="tree-node-office"]/span[@class="rct-checkbox"]'
+      );
+
       await page.goto("https://demoqa.com/checkbox");
-      
+
       await toggleButton1.click();
       await toggleButton2.click();
       await toggleButton3.click();
       await toggleButton4.click();
       await toggleButton5.click();
       await toggleButton6.click();
-      
+
       await checkboxExcelFile.check();
       await expect(checkboxExcelFile).toBeChecked();
-      await expect(verifyDisplaying).toContainText('excelFile');
+      await expect(verifyDisplaying).toContainText("excelFile");
       await checkboxExcelFile.uncheck();
-      
+
       await checkboxWordFile.check();
       await expect(checkboxWordFile).toBeChecked();
-      await expect(verifyDisplaying).toContainText('wordFile');
+      await expect(verifyDisplaying).toContainText("wordFile");
       await checkboxWordFile.uncheck();
 
       await checkboxDownloads.check();
       await expect(checkboxDownloads).toBeChecked();
-      await expect(verifyDisplayingDownloads).toContainText('downloads');
+      await expect(verifyDisplayingDownloads).toContainText("downloads");
       await checkboxDownloads.uncheck();
 
       await checkboxGeneral.check();
       await expect(checkboxGeneral).toBeChecked();
-      await expect(verifyDisplaying).toContainText('general');
+      await expect(verifyDisplaying).toContainText("general");
       await checkboxGeneral.uncheck();
-      
+
       await checkboxClassified.check();
       await expect(checkboxClassified).toBeChecked();
-      await expect(verifyDisplaying).toContainText('classified');
+      await expect(verifyDisplaying).toContainText("classified");
       await checkboxClassified.uncheck();
 
       await checkboxPrivate.check();
       await expect(checkboxPrivate).toBeChecked();
-      await expect(verifyDisplaying).toContainText('private');
+      await expect(verifyDisplaying).toContainText("private");
       await checkboxPrivate.uncheck();
 
       await checkboxPublic.check();
       await expect(checkboxPublic).toBeChecked();
-      await expect(verifyDisplaying).toContainText('public');
+      await expect(verifyDisplaying).toContainText("public");
       await checkboxPublic.uncheck();
 
       await checkboxOffice.check();
       await expect(checkboxOffice).toBeChecked();
-      await expect(verifyDisplayingOffice).toContainText('office');
+      await expect(verifyDisplayingOffice).toContainText("office");
       await checkboxOffice.uncheck();
     }
   );
